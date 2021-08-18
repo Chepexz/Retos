bf18d721248939ae9919b9c02e4b53295083bf24
Chepexz <jose.la@culiacan.tecnm.mx>
2021-08-18 04:53:02 +0000

    Angulo terminado

--- 01Angulos/00EciacionesLineales/src/exercise.py
+++ 01Angulos/00EciacionesLineales/src/exercise.py
@@ -1,23 +1,17 @@
 import os
 def main():
     os.system("clear")
-    a = float(input("Valor para 'a'"))
-    b = float(input("Valor para 'b'"))
-    c = float(input("Valor para 'c'"))
-    d = float(input("Valor para 'd'"))
-    e = float(input("Valor para 'e'"))
-    f = float(input("Valor para 'f'"))
-
-    if (a * e - b * d) != 0 :
-    x = (c * e - b * f) / (a * e - b * d)
-    y = (a * f - c * d) / (a * e - b * d)
-    print (f"La solucion al sistema es x= {x} e y= {y}")
-    #print ("La solucion al sistema es x= %d e y= %d" %(x, y)) sin decimales
-else :
-    if (d / a) * c == f :
-        print ("El sistema tiene infinitas soluciones")
-    else:
-        print ("El sistema no tiene solución")
+    angulo = int(input("Tamaño del Ángulo: "))
+    if angulo < 90:
+        print("El angulo es Agudo")
+    elif angulo == 90:
+        print("El angulo es Recto")
+    elif angulo > 90 and angulo < 180:
+        print("El angulo es Obtuso")
+    elif angulo == 180:
+        print("El angulo es Llano")
+    elif angulo>180 and angulo<360:
+        print("El angulo es Cóncavo")
         
 if __name__=='__main__':
     main()
